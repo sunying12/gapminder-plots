f7ba21dc1ca6a291526e42d94fc3ea156a4acdd4
“Ying <sunying12@gmail.com>
2017-08-06 09:53:22 -0700

    add code to read gapminder data

---
+++ gapminder.R
@@ -0,0 +1,3 @@
+download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv","data/gapminder-FiveYears.csv")
+
+gapminder <-read.csv("data/gapminder-FiveYears.csv")

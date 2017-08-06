f848e582832a188192688a0959c8ccf4d7492776
“Ying <sunying12@gmail.com>
2017-08-06 10:04:12 -0700

    stuff and things and this is hard

--- gapminder-plots.R
+++ gapminder-plots.R
@@ -1,3 +1,8 @@
 download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv","data/gapminder-FiveYears.csv")
 
 gapminder <-read.csv("data/gapminder-FiveYears.csv")
+
+library (ggplot2)
+
+ggplot(gapminder, aes(x= gdpPercap, y =lifeExp)) + 
+  geom_point()

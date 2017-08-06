cf110ccca2151125e496645c2471d4567f7dd40e
“Ying <sunying12@gmail.com>
2017-08-06 12:00:51 -0700

    fix#2 : add data for :jp:

--- gapminder-plots.R
+++ gapminder-plots.R
@@ -8,3 +8,13 @@ ggplot(gapminder , aes(x= gdpPercap, y =lifeExp)) +
   geom_point(aes(color = continent), shape = "@") +
   geom_smooth(color = "tomato", method = "lm")
 
+ggplot (gapminder, aes(x= gdpPercap, y = lifeExp )) + 
+  geom_point( color = "blue", aes(color=continent)) + 
+  scale_x_log10() + 
+  geom_smooth(color = "tomato", method = "lm")
+
+japan_gapminder <- gapminder[gapminder$country == "Japan", ]
+
+ggplot(japan_gapminder, aes(x=year,y= lifeExp)) +
+  geom_line(color = "coral") +
+  xlab("Life expectancy") + ylab ("Year")
--- gapminder.R
+++
@@ -1,3 +0,0 @@
-download.file("https://raw.githubusercontent.com/swcarpentry/r-novice-gapminder/gh-pages/_episodes_rmd/data/gapminder-FiveYearData.csv","data/gapminder-FiveYears.csv")
-
-gapminder <-read.csv("data/gapminder-FiveYears.csv")
